e2c8c331aeb5516ff42caa3b1c57190026c94b8b
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2024-09-26 22:02:12 -0700

    parse API version

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,15 @@ fn main() {
 
                 let mut msg = msg.as_slice();
                 let _api_key = msg.get_i16();
-                let _api_version = msg.get_i16();
+                let api_version = msg.get_i16();
                 let correlation_id = msg.get_i32();
 
                 let mut response = Vec::new();
-                response.put_i32(0);
+                response.put_i32(0); // todo: message length
                 response.put_i32(correlation_id);
+                if api_version < 0 || api_version > 4 {
+                    response.put_i16(35);
+                }
                 stream.write(&response).unwrap();
             }
             Err(e) => {
