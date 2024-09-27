e5f140281f94b4e18d8dcbf53dcacc13bda8a2dd
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2024-09-26 21:56:26 -0700

    parse correlation ID

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use std::net::TcpListener;
+use bytes::{Buf, BufMut};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -7,8 +11,24 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                let mut buf = [0; 4];
+                stream.read_exact(&mut buf).unwrap();
+                let len = u32::from_be_bytes(buf) as usize;
+
+                let mut msg = vec![0; len];
+                stream.read_exact(&mut msg).unwrap();
+
+                let mut msg = msg.as_slice();
+                let _api_key = msg.get_i16();
+                let _api_version = msg.get_i16();
+                let correlation_id = msg.get_i32();
+
+                let mut response = Vec::new();
+                response.put_i32(0);
+                response.put_i32(correlation_id);
+                stream.write(&response).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
